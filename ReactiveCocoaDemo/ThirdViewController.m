ac3c9fa53df07dea4890709e5d030f081168155d
陈宇峰 <yufengcode@gmail.com>
2016-05-17 23:16:10 +0800

    third

--- ReactiveCocoaDemo/ThirdViewController.m
+++ ReactiveCocoaDemo/ThirdViewController.m
@@ -8,6 +8,7 @@
 //
 
 #import "ThirdViewController.h"
+#import "ReactiveCocoa.h"
 
 @interface ThirdViewController ()<UITableViewDelegate,UITableViewDataSource>
 
@@ -21,7 +22,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.myArray = [[NSMutableArray alloc] initWithObjects:@"1",@"2", nil];
+    self.myArray = [[NSMutableArray alloc] initWithObjects:@"冷信号",@"热信号", nil];
     self.myTableView = [[UITableView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.myTableView.delegate = self;
     self.myTableView.dataSource = self;
@@ -48,8 +49,91 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     [tableView deselectRowAtIndexPath:indexPath animated:NO];
-    NSLog(@"1");
-    
+    switch (indexPath.row) {
+        case 0:
+            [self coldSignal];
+            break;
+        case 1:
+            [self hotSignal];
+            break;
+        default:
+            break;
+    }
+
+}
+
+#pragma mark - Function
+
+// 冷信号
+- (void)coldSignal
+{
+    RACSignal *signal = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [subscriber sendNext:@1];
+        [subscriber sendNext:@2];
+        [subscriber sendNext:@3];
+        [subscriber sendCompleted];
+
+        return nil;
+    }];
+    NSLog(@"Signal was created");
+
+    [[RACScheduler mainThreadScheduler] afterDelay:0.1 schedule:^{
+        [signal subscribeNext:^(id x) {
+            NSLog(@"Subscriber 1 receive:%@",x);
+        }];
+    }];
+
+    [[RACScheduler mainThreadScheduler] afterDelay:1 schedule:^{
+        [signal subscribeNext:^(id x) {
+            NSLog(@"Subscriber 2 receive:%@",x);
+        }];
+    }];
+}
+
+// 热信号
+- (void)hotSignal
+{
+    RACMulticastConnection *connection = [[RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [[RACScheduler mainThreadScheduler] afterDelay:1 schedule:^{
+            [subscriber sendNext:@1];
+        }];
+        [[RACScheduler mainThreadScheduler] afterDelay:2 schedule:^{
+            [subscriber sendNext:@2];
+        }];
+        [[RACScheduler mainThreadScheduler] afterDelay:3 schedule:^{
+            [subscriber sendNext:@3];
+        }];
+        [[RACScheduler mainThreadScheduler] afterDelay:4 schedule:^{
+            [subscriber sendCompleted];
+        }];
+
+        return nil;
+    }] publish];
+
+    [connection connect];
+
+    RACSignal *signal = connection.signal;
+    NSLog(@"Signal was created");
+
+    [[RACScheduler mainThreadScheduler] afterDelay:1.5 schedule:^{
+        [signal subscribeNext:^(id x) {
+            NSLog(@"Subscriber 1 receive:%@",x);
+        }];
+    }];
+    [[RACScheduler mainThreadScheduler] afterDelay:2.5 schedule:^{
+        [signal subscribeNext:^(id x) {
+            NSLog(@"Subscriber 2 receive:%@",x);
+        }];
+    }];
+
 }
 
 @end
+
+
+
+
+
+
+
+
